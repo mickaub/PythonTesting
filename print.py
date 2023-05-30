2c968939bd63f07364f95c018e7c1dc55eec950d
mickaub <mickaub3@gmail.com>
2023-05-30 13:10:46 +1000

    init

---
+++ print.py
@@ -0,0 +1,14 @@
+print('Hello')
+x = 3
+if x == 3 :
+    print(x)
+rawstr = input('enter a number:')
+try:
+    ival = int(rawstr)
+except:
+    ival = -1
+
+if ival > 0 :
+    print('Nice')
+else:
+    print('Not a number')
