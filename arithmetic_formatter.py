bae82c7677f99480eda93dd0dd1f6a2d9428db39
mickaub <mickaub3@gmail.com>
2023-07-19 14:38:55 +1000

    init

---
+++ arithmetic_formatter.py
@@ -0,0 +1,9 @@
+#ARITHMETIC ARRANGER: FCC PYTHON PROJECT 1
+#arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True) true is answers inc
+#32
+#+ 8
+#----
+#40
+# print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+# limit of 5 problems, + and - only, digits only, four digits max for numbers
+#numbers should be right aligned, dashes between each problem
