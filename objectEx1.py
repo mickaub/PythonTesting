35efd9cf6980adcb7d93f99ffde0137e05ce77d4
mickaub <mickaub3@gmail.com>
2023-07-19 14:38:37 +1000

    misc

--- objectEx1.py
+++ objectEx1.py
@@ -1,5 +1,5 @@
 class CountingUp :
-    x = 0
+    x = 3
     adder = 0
     def __init__(self,num):
         self.adder = num
@@ -9,5 +9,6 @@ class CountingUp :
         print(a.x)
 
 an = CountingUp(3)
+print(3)
 an.increase()
 an.increase()
