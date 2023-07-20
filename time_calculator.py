a1db66e3edb0acb7d814178d5655bda0e3b94aa2
mickaub <mickaub3@gmail.com>
2023-07-20 12:29:42 +1000

    add: instructions

--- time_calculator.py
+++ time_calculator.py
@@ -1,1 +1,13 @@
 #FCC Project 2
+""" #do three quotes in front of text for long comment commencement
+Write a function named add_time that takes in two required parameters and one
+optional parameter:
+A start time in 12 hour clock format with AM/PM: HH:MM AM/PM
+A duration time in H:MM format
+Optional: Starting day of the week, case insensitive
+
+Function should add duration of time to the start time/day and return the result.
+If the result is in the next day, (next day) should be added to the end of the result.
+If the result is more than one day later, it should show (x days later).
+If optional argument day is given, result should also include day before x days later.
+"""
