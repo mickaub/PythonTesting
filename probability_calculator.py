1c74c8c7194af1d2b3879236d259ff41792bf88d
mickaub <mickaub3@gmail.com>
2023-07-28 14:27:31 +1000

    add: began adding instructions

--- probability_calculator.py
+++ probability_calculator.py
@@ -1,1 +1,20 @@
 #FCC Project 5
+"""
+Creata a Hat Class with variable amount of arguments (*args).
+Quantity will always be at least 1.
+Arguments should be converted into contents instance variable,
+with contents being a list of strings that contain the quantity
+of each category sequentially.
+eg. Hat(yellow=3)
+contents = ["yellow","yellow","yellow"]
+
+draw method:
+accepts argument indicating the number of balls to draw out of hat.
+should remove balls at random from contents and return the results
+as a new list of strings.
+If the number of balls to draw exceeds the available quantity,
+return all of the balls.
+
+seperate experiment function:
+#unfinsihed
+"""
